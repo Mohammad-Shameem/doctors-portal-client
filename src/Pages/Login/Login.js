ab4fed2d9f4269b8737e56a2e5fa072d69b11a89
Mohammad-Shameem <mehmetsaki789@gmail.com>
2022-05-14 10:12:51 +0300

    googole authentication impllemented

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  return (
+    <div className="grid grid-cols-1 justify-items-center items-center h-screen ">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="text-center text-2xl font-bold ">Login</h2>
+          <form>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text text-base font-normal">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+              />
+              <label className="label">
+                <span className="label-text text-base font-normal">
+                  Password
+                </span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+              />
+              <label className="label">
+                <span className="label-text text-xs ">Forgot password?</span>
+              </label>
+            </div>
+          </form>
+
+          <div className="divider w-full">OR</div>
+          <button
+            onClick={() => signInWithGoogle()}
+            className="btn btn-outline"
+          >
+            CONTINUE WITH GOOGLE
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
